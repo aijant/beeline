3f1e314a31fe84a898ecb65c2fe9e1b37494fdc6
Aizhan Tilek <aijanm01@gmail.com>
2021-07-08 16:18:39 +0600

    Set up project

---
+++ bee-ivi/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+$color: red;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $color;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: $color;
+
+    &.router-link-exact-active {
+      color: $color;
+    }
+  }
+}
+</style>
